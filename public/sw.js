258220826801b5b46cf6f9a901c74b43d074d6cb
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2024-10-07 03:49:00 +0900

    Revert "処理する場所を変更"
    This reverts commit 8851fd48adbe1b055ace8ea410609f6c16f8a6e8.

--- public/sw.js
+++ public/sw.js
@@ -2,34 +2,34 @@ import { precacheAndRoute } from 'workbox-precaching';
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-self.addEventListener("fetch", async (event) => {
+self.addEventListener("fetch", (event) => {
     if (event.request.url.endsWith('upload') && event.request.method === 'POST') {
-        const formData = await event.request.formData();
-        const files = formData.getAll("files");
         event.respondWith(
-            (() => {
+            (async () => {
+                const formData = await event.request.formData();
+                const files = formData.getAll("files");
                 if (!files.length) {
                     return Response.redirect("failed", 303);
                 }
+                files.forEach(async file => {
+                    const cache = await caches.open('upload-files');
+                    const response = new Response(file, {
+                        headers: { 'Content-Type': file.type }
+                    });
+                    await cache.put(`upload/${file.name}`, response);
+
+                    // 必要に応じてクライアントにメッセージを送る
+                    self.clients.matchAll().then(clients => {
+                        clients.forEach(client => client.postMessage({
+                            fileName: file.name,
+                            fileUrl: `upload/${file.name}`
+                        }));
+                    });
+
+                });
                 return Response.redirect("upload", 303);
             })(),
         );
-        files.forEach(async file => {
-            const cache = await caches.open('upload-files');
-            const response = new Response(file, {
-                headers: { 'Content-Type': file.type }
-            });
-            await cache.put(`upload/${file.name}`, response);
-
-            // 必要に応じてクライアントにメッセージを送る
-            self.clients.matchAll().then(clients => {
-                clients.forEach(client => client.postMessage({
-                    fileName: file.name,
-                    fileUrl: `upload/${file.name}`
-                }));
-            });
-
-        });
     } else if (event.request.url.includes("/callback")) {
         event.respondWith(fetch("index.html"));
     }
