eab7bfdb2ff2e56db1819a78c128afb91b55ca0b
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2024-10-07 02:48:00 +0900

    build対象HTMLファイルを設定

--- vite.config.js
+++ vite.config.js
@@ -1,73 +1,83 @@
+import { resolve } from 'path'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default {
     base: '/MisskeyFileUploadPWA/',
     plugins: [
-      VitePWA({
-        registerType: 'autoUpdate',
-        strategies: 'injectManifest',
-        injectRegister: 'auto',
-        manifest: {
-            "name": "Misskey ファイルアップロードPWA",
-            "short_name": "アップロード",
-            "description": "Misskeyのドライブへファイルを簡単に共有します。",
-            "start_url": "/MisskeyFileUploadPWA/",
-            "display": "standalone",
-            "theme_color": "#101010",
-            "background_color": "#1f1f1f",
-            "lang": "ja-JP",
-            "dir": "ltr",
-            "icons": [
-                {
-                    "src": "icon/icon-512x512.png",
-                    "sizes": "512x512",
-                    "purpose": "maskable"
-                },
-                {
-                    "src": "icon/icon-192x192.png",
-                    "sizes": "192x192",
-                    "purpose": "maskable"
-                },
-                {
-                    "src": "icon/icon-144x144.png",
-                    "sizes": "144x144",
-                    "purpose": "maskable"
-                },
-                {
-                    "src": "icon/icon-144x144a.png",
-                    "sizes": "144x144",
-                    "purpose": "any"
-                },
-                {
-                    "src": "icon/icon-96x96.png",
-                    "sizes": "96x96",
-                    "purpose": "maskable"
-                },
-                {
-                    "src": "icon/icon-72x72.png",
-                    "sizes": "72x72",
-                    "purpose": "maskable"
-                },
-                {
-                    "src": "icon/icon-48x48.png",
-                    "sizes": "48x48",
-                    "purpose": "maskable"
-                }
-            ],
-            "share_target": {
-                "action": "/MisskeyFileUploadPWA/upload",
-                "method": "POST",
-                "enctype": "multipart/form-data",
-                "params": {
-                    "files": [
-                        {
-                        "name": "files",
-                        "accept": ["*/*",".*"]
-                        }
-                    ]
+        VitePWA({
+            registerType: 'autoUpdate',
+            strategies: 'injectManifest',
+            injectRegister: 'auto',
+            manifest: {
+                "name": "Misskey ファイルアップロードPWA",
+                "short_name": "アップロード",
+                "description": "Misskeyのドライブへファイルを簡単に共有します。",
+                "start_url": "/MisskeyFileUploadPWA/",
+                "display": "standalone",
+                "theme_color": "#101010",
+                "background_color": "#1f1f1f",
+                "lang": "ja-JP",
+                "dir": "ltr",
+                "icons": [
+                    {
+                        "src": "icon/icon-512x512.png",
+                        "sizes": "512x512",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "icon/icon-192x192.png",
+                        "sizes": "192x192",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "icon/icon-144x144.png",
+                        "sizes": "144x144",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "icon/icon-144x144a.png",
+                        "sizes": "144x144",
+                        "purpose": "any"
+                    },
+                    {
+                        "src": "icon/icon-96x96.png",
+                        "sizes": "96x96",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "icon/icon-72x72.png",
+                        "sizes": "72x72",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "icon/icon-48x48.png",
+                        "sizes": "48x48",
+                        "purpose": "maskable"
+                    }
+                ],
+                "share_target": {
+                    "action": "/MisskeyFileUploadPWA/upload",
+                    "method": "POST",
+                    "enctype": "multipart/form-data",
+                    "params": {
+                        "files": [
+                            {
+                                "name": "files",
+                                "accept": ["*/*", ".*"]
+                            }
+                        ]
+                    }
                 }
             }
-        }
-      })
-    ]
+        })
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                top: resolve(__dirname, 'index.html'),
+                upload: resolve(__dirname, 'upload.html'),
+                failed: resolve(__dirname, 'failed.html'),
+            },
+        },
+    },
 }
